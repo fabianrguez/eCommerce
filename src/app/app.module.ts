d9bf1ac2c5dae31a5f1642386e03342afc09634c
fabianrguez <Fr01041993>
2018-05-04 11:33:26 +0100

    create directive for change color on scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductsModule } from './components/products/products.module';
 import {AppRoutingModule} from './app-routing.module';
+import { ChangeColorOnScrollDirective } from './directives/change-color-on-scroll.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChangeColorOnScrollDirective
   ],
   imports: [
     BrowserModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,21 +7,9 @@ import {Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild} from 
 })
 export class HeaderComponent implements OnInit {
 
-  @ViewChild('navbar') navbar: ElementRef;
-
-  constructor(private renderer: Renderer2) { }
-
-  ngOnInit() {
+  constructor() {
   }
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (number > 20) {
-      this.renderer.addClass(this.navbar.nativeElement, 'scrolled');
-      this.renderer.addClass(this.navbar.nativeElement, 'text-white');
-    } else if (number < 20) {
-      this.renderer.removeClass(this.navbar.nativeElement, 'scrolled');
-    }
+  ngOnInit() {
   }
 }
