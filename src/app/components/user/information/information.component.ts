00f64743ad270b5e1f27e74919fb7204bbe78e61
fabianrguez <Fr01041993>
2018-05-15 16:08:13 +0100

    validate user information form

--- src/app/components/user/information/information.component.ts
+++ src/app/components/user/information/information.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {AuthService} from '../../../services/auth.service';
 import {User} from '../../../models/user';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-information',
@@ -9,16 +10,91 @@ import {User} from '../../../models/user';
 })
 export class InformationComponent implements OnInit {
 
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('lastName') lastName: ElementRef;
+  @ViewChild('email') email: ElementRef;
+  @ViewChild('address') address: ElementRef;
+  @ViewChild('location') location: ElementRef;
+  @ViewChild('postalCode') postalCode: ElementRef;
+
   currentUser: User;
   logged: boolean;
+  userForm: FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private formsBuilder: FormBuilder,
+              private renderer: Renderer2) {
   }
 
   ngOnInit() {
+    this.initializeUserForm();
     this.authService.currentUser.subscribe(user => {
       this.logged = user != null;
       this.currentUser = this.authService.getUserLogged();
     });
+  }
+
+  private initializeUserForm() {
+    this.userForm = this.formsBuilder.group({
+      'name': new FormControl('', Validators.required),
+      'lastName': new FormControl('', Validators.required),
+      'email': new FormControl('', Validators.compose([Validators.email, Validators.required])),
+      'address': new FormControl('', Validators.required),
+      'location': new FormControl('', Validators.required),
+      'postalCode': new FormControl('', Validators.required),
+    });
+  }
+
+  isNameInvalid() {
+    return this.checkFieldInvalid('name', this.name);
+  }
+
+  isLastnameInvalid() {
+    return this.checkFieldInvalid('lastName', this.lastName);
+  }
+
+  isEmailInvalid() {
+    return this.checkFieldInvalid('email', this.email);
+  }
+
+  isAddressInvalid() {
+    return this.checkFieldInvalid('address', this.address);
+  }
+
+  isLocationInvalid() {
+    return this.checkFieldInvalid('location', this.location);
+  }
+
+  isPostalCodeInvalid() {
+    return this.checkFieldInvalid('postalCode', this.postalCode);
+  }
+
+  private checkFieldInvalid(field: string, element: ElementRef) {
+    if (!this.isFieldValid(field) && this.isFieldDirty(field)) {
+      this.addInvalidClass(element);
+      return true;
+    }
+    this.removeInvalidClassIfNecessary(element);
+    return false;
+  }
+
+  private isFieldValid(field: string) {
+    return this.userForm.controls[field].valid;
+  }
+
+  private isFieldDirty(field: string) {
+    return this.userForm.controls[field].dirty;
+  }
+
+  private addInvalidClass(element: ElementRef) {
+    if (element != null) {
+      this.renderer.addClass(element.nativeElement, 'is-invalid');
+    }
+  }
+
+  private removeInvalidClassIfNecessary(element: ElementRef) {
+    if (element != null && element.nativeElement.classList.contains('is-invalid')) {
+      this.renderer.removeClass(element.nativeElement, 'is-invalid');
+    }
   }
 }
--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AccountComponent } from './account/account.component';
 import { InformationComponent } from './information/information.component';
 
@@ -10,7 +10,8 @@ import { InformationComponent } from './information/information.component';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     LoginComponent,
